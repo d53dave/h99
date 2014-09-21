4e0069ccb073a6904140b417a6bab472b2c4a8b1
d53dave <dave@d53dev.net>
2014-09-21 21:16:05 +0200

    Added more solutions

---
+++ h09.hs
@@ -0,0 +1,8 @@
+-- Pack consecutive duplicates of list elements into sublists.
+pack :: (Eq a) => [a] -> [a]
+
+pack [] = []
+pack (x:xs) = let (first,rest) = span (==x) xs
+               in (x:first) : pack rest
+
+-- Data.List has a group function. does the same thing.
